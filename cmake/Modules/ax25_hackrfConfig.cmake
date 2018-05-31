0585b903b2884d508e24f528d1584a106a8d8b91
Eugenio Rivera <eugeniorrivera@gmail.com>
2018-05-30 17:17:19 -0700

    First commit

---
+++ cmake/Modules/ax25_hackrfConfig.cmake
@@ -0,0 +1,30 @@
+INCLUDE(FindPkgConfig)
+PKG_CHECK_MODULES(PC_AX25_HACKRF ax25_hackrf)
+
+FIND_PATH(
+    AX25_HACKRF_INCLUDE_DIRS
+    NAMES ax25_hackrf/api.h
+    HINTS $ENV{AX25_HACKRF_DIR}/include
+        ${PC_AX25_HACKRF_INCLUDEDIR}
+    PATHS ${CMAKE_INSTALL_PREFIX}/include
+          /usr/local/include
+          /usr/include
+)
+
+FIND_LIBRARY(
+    AX25_HACKRF_LIBRARIES
+    NAMES gnuradio-ax25_hackrf
+    HINTS $ENV{AX25_HACKRF_DIR}/lib
+        ${PC_AX25_HACKRF_LIBDIR}
+    PATHS ${CMAKE_INSTALL_PREFIX}/lib
+          ${CMAKE_INSTALL_PREFIX}/lib64
+          /usr/local/lib
+          /usr/local/lib64
+          /usr/lib
+          /usr/lib64
+)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(AX25_HACKRF DEFAULT_MSG AX25_HACKRF_LIBRARIES AX25_HACKRF_INCLUDE_DIRS)
+MARK_AS_ADVANCED(AX25_HACKRF_LIBRARIES AX25_HACKRF_INCLUDE_DIRS)
+
---
+++ swig/ax25_hackrf_swig.i
@@ -0,0 +1,13 @@
+/* -*- c++ -*- */
+
+#define AX25_HACKRF_API
+
+%include "gnuradio.i"			// the common stuff
+
+//load generated python docstrings
+%include "ax25_hackrf_swig_doc.i"
+
+%{
+%}
+
+
